fded422f6903adf91a12ee3fd385cd299d168997
JoeYip13 <>
2023-07-05 12:36:48 +0000

    Fixed typo error in settings.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ci-joe-yip-django-blog-2c7016a872a6.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['ci-joe-yip-django-blog-2c7016a872a6.herokuapp.com', 'localhost']
 
 
 # Application definition
